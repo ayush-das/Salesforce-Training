df215ad641bfc68e4b36137506932c69af078672
Ayush Das <69136510+ayush-das@users.noreply.github.com>
2024-02-10 12:28:37 +0530

    Add files via upload

---
+++ loopingData/loopingData.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+
+export default class LoopingData extends LightningElement {
+    Customer = [{
+            id: 1,
+            name: 'Ayush',
+            Company: 'CTS'
+
+        },
+
+        {
+            id: 2,
+            name: 'Vasavi',
+            Company: 'TP'
+        }
+
+    ]
+
+
+}
